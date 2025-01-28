6565160cb71ea2163907a39e40e87994b3f56c27
dulina2004 <dulinagunasinghe@gmail.com>
2025-01-28 11:15:38 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TodoList from "./assets/ToDoList";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return <TodoList />;
+}
+
+export default App;
---
+++ src/assets/ToDoList.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function TodoList() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(event) {}
+
+    function addTask() {}
+
+    function deleteTask(index) {}
+
+    function moveTaskUp(index) {}
+
+    return (
+        <>
+            <div></div>
+        </>
+    );
+}
+
+export default TodoList;
